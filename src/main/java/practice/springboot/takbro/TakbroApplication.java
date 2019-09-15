5a9ff0cdf2964d9cfc512672e26dddc4bfd90dc6
tak <env.tak@gmail.com>
2019-09-15 14:22:07 +0900

    feat(init): add default project of spring-boot

---
+++ src/main/java/practice/springboot/takbro/TakbroApplication.java
@@ -0,0 +1,13 @@
+package practice.springboot.takbro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakbroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakbroApplication.class, args);
+	}
+
+}
