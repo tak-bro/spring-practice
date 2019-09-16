118d8f67d23a0fa46683f8eb2e9ce066ec23d62e
tak <env.tak@gmail.com>
2019-09-16 19:17:52 +0900

    feat: add default rest-api

--- src/main/java/practice/springboot/takbro/controller/MultiplicationController.java
+++ src/main/java/practice/springboot/takbro/controller/MultiplicationController.java
@@ -1,10 +1,14 @@
 package practice.springboot.takbro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import practice.springboot.takbro.domain.Multiplication;
 import practice.springboot.takbro.service.MultiplicationService;
 
 @RestController
+@RequestMapping("/multiplications")
 public class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
@@ -12,5 +16,10 @@ public class MultiplicationController {
     @Autowired
     public MultiplicationController(final MultiplicationService multiplicationService) {
         this.multiplicationService = multiplicationService;
+    }
+
+    @GetMapping("/random")
+    Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
     }
 }
---
+++ src/test/java/practice/springboot/takbro/controller/MultiplicationResultAttemptControllerTest.java
@@ -0,0 +1,78 @@
+package practice.springboot.takbro.controller;
+import static practice.springboot.takbro.controller.MultiplicationResultAttemptController.ResultResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import practice.springboot.takbro.domain.Multiplication;
+import practice.springboot.takbro.domain.MultiplicationResultAttempt;
+import practice.springboot.takbro.domain.User;
+import practice.springboot.takbro.service.MultiplicationService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MultiplicationResultAttemptController.class)
+public class MultiplicationResultAttemptControllerTest {
+
+    @MockBean
+    private MultiplicationService multiplicationService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private JacksonTester<MultiplicationResultAttempt> jsonResult;
+    private JacksonTester<ResultResponse> jsonResponse;
+
+    @Before
+    public void setUp() {
+        JacksonTester.initFields(this, new ObjectMapper());
+    }
+
+    @Test
+    public void postResultReturnCorrect() throws Exception {
+        genericParameterizedTest(true);
+    }
+
+    @Test
+    public void postResultReturnNotCorrect() throws Exception {
+        genericParameterizedTest(false);
+    }
+
+    void genericParameterizedTest(final boolean correct) throws Exception {
+        // given
+        given(multiplicationService
+                .checkAttempt(any(MultiplicationResultAttempt.class)))
+                .willReturn(correct);
+
+        User user = new User("John");
+        Multiplication multiplication = new Multiplication(50, 70);
+        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3500);
+
+        // when
+        MockHttpServletResponse response = mvc.perform(post("/results")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonResult.write(attempt).getJson()))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonResponse.write(new ResultResponse(correct)).getJson());
+    }
+
+
+}
